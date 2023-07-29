56fb2f5a1bfa3ce92a7349f787f0271db054281b
Romina-Ramirez <romina.maite.rt@gmail.com>
2023-07-29 10:52:01 -0500

    Taller 35

---
+++ src/modules/estudiante/components/DatosEstudiante.vue
@@ -0,0 +1,23 @@
+<template>
+    <input v-model="cedula" type="text" placeholder="Ingrese la cédula...">
+    <button @click="consultarEstudiante">Consultar</button>
+</template>
+
+<script>
+import { obtenerEstudianteFachada } from "../helpers/EstudianteCliente.js";
+export default {
+  data() {
+    return {
+      cedula: null,
+    };
+  },
+  methods: {
+    async consultarEstudiante() {
+      await obtenerEstudianteFachada(this.cedula);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
